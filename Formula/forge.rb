7df59fdcb32636f78f61743a33223b3d649a2d5a
Dhanush Reddy Chilakala <dhanush.actual1@gmail.com>
2026-06-01 23:51:58 -0500

    chore: sync formula to v0.1.3

--- Formula/forge.rb
+++ Formula/forge.rb
@@ -1,8 +1,8 @@
 class Forge < Formula
   desc "Idea to product in one command"
   homepage "https://github.com/Ddundee/forge"
-  url "https://github.com/Ddundee/forge/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "e10df30f9ef02a62a624b5afc2c2438022d4c5b7f2d1bc6fddc77aff8c283882"
+  url "https://github.com/Ddundee/forge/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "f16f650ff23c4bdeb72a272846b1667b38760c3c528d0e0ab6546a661f842296"
   license "MIT"
 
   depends_on "python@3.11"
